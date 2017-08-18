12919640c3429bf4e69558df679f4743858d7cf4
ninuxGithub <1732768613@qq.com>
2017-08-18 14:23:11 +0800

    build PageRequest

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -8,6 +8,8 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -21,7 +23,7 @@ import com.example.demo.repository.UserRepository;
 public class IndexController {
 
 	private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
-	
+
 	private static final Integer PAGECONT = 10;
 
 	@Autowired
@@ -33,47 +35,44 @@ public class IndexController {
 		return "index";
 	}
 
-
 	@ResponseBody
 	@RequestMapping("/test.json")
-	public Map<String, Object> loadUserPages (@RequestParam(name="pageNo", defaultValue="1") Integer pageNo) {
+	public Map<String, Object> loadUserPages(@RequestParam(name = "pageNo", defaultValue = "1") Integer pageNo) {
 		long start = System.currentTimeMillis();
-		//pageNo-1非常重要
-		PageRequest pageRequest = new PageRequest(pageNo-1, PAGECONT);
+		// pageNo-1非常重要, 默认是根据ID 升序排序的
+		PageRequest pageRequest = new PageRequest(pageNo - 1, PAGECONT, new Sort(Direction.ASC, "id"));
 		Page<User> userPages = userRepository.findAll(pageRequest);
-//		List<User> users = userRepository.findAll();
+		// List<User> users = userRepository.findAll();
 		Map<String, Object> map = new HashMap<>();
 		map.put("userPages", userPages);
 		map.put("pageNo", pageNo);
 		long end = System.currentTimeMillis();
-		logger.info("spend time :{} ------- pageNo is :{}",(end-start), pageNo);
+		logger.info("spend time :{} ------- pageNo is :{}", (end - start), pageNo);
 		return map;
 	}
-	
+
 	@ResponseBody
 	@RequestMapping("/newUser.json")
 	public User inputUser(@RequestParam Long id) {
-		if(null == id){
+		if (null == id) {
 			return new User();
-		}else{
+		} else {
 			return userRepository.findOne(id);
 		}
 	}
-	
-	
+
 	@RequestMapping(value = "/addUser", method = RequestMethod.POST)
 	public String inputUser(User user) {
-		logger.info("user is : "+user.toString());
+		logger.info("user is : " + user.toString());
 		user.setRole("ROLE_ADMIN");
-		if(user.getId()==null){
+		if (user.getId() == null) {
 			userRepository.save(user);
-		}else{
+		} else {
 			userRepository.saveAndFlush(user);
 		}
 		return "redirect:/";
 	}
-	
-	
+
 	@RequestMapping(value = "/deleteUser", method = RequestMethod.POST)
 	public String deleteUser(@RequestParam Long id) {
 		logger.info("delete id is :{}", id);
@@ -85,20 +84,20 @@ public class IndexController {
 	@RequestMapping(value = "/getUser", method = RequestMethod.POST)
 	public User getUser(@RequestParam Long id) {
 		logger.info("getUser id is : {}", id);
-		if(null == id){
-			return new  User();
+		if (null == id) {
+			return new User();
 		}
 		User user = userRepository.findOne(id);
 		return user;
 	}
-//	@ResponseBody
-//	@RequestMapping(value = "/getUser/{id}", method = RequestMethod.GET)
-//	public User getUser(@PathVariable(required=false) Long id) {
-//		logger.info("getUser id is : {}", id);
-//		if(null == id|| id==-1){
-//			return new  User();
-//		}
-//		User user = userRepository.findOne(id);
-//		return user;
-//	}
+	// @ResponseBody
+	// @RequestMapping(value = "/getUser/{id}", method = RequestMethod.GET)
+	// public User getUser(@PathVariable(required=false) Long id) {
+	// logger.info("getUser id is : {}", id);
+	// if(null == id|| id==-1){
+	// return new User();
+	// }
+	// User user = userRepository.findOne(id);
+	// return user;
+	// }
 }
