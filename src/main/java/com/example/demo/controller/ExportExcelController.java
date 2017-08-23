a63308adc2fd6fa8fbfc70360e23fecbccab1c9f
ninuxGithub <1732768613@qq.com>
2017-08-23 17:46:02 +0800

    excel 多表头需要修改

--- src/main/java/com/example/demo/controller/ExportExcelController.java
+++ src/main/java/com/example/demo/controller/ExportExcelController.java
@@ -1,14 +1,20 @@
 package com.example.demo.controller;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.poi.hssf.usermodel.HSSFCellStyle;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.CellRangeAddress;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -40,11 +46,48 @@ public class ExportExcelController {
 		List<User> users = userRepository.findAll();
 		try {
 			HSSFWorkbook wb = new HSSFWorkbook();
+			
 			HSSFSheet sheet = wb.createSheet("风控报表1");
+			
 			sheet.setDefaultColumnWidth(15);// 15个字符
 
 			ExcelUtil.drawExcel(orders, wb, sheet, Order.class);
 
+			ExcelUtil.increaseRow();// 换行//每个填充之间加入一个空行
+			
+//			for(int i=0; i<7; i++){
+//				CellRangeAddress region = new CellRangeAddress(ExcelUtil.getLocalRow(), ExcelUtil.getLocalRow()+1 , (short) i, (short)i ); 
+//				region.
+//			}
+			
+			String[] head0 = new String[]{"日期","组合名称","组合收益率","业绩基准","净值损线"};
+			String[] head1 = new String[]{"本期","今年以来","本期","今年以来"};
+			
+			Row row = null;
+			Cell cell = null;
+			HSSFCellStyle titleStyle = ExcelUtil.titleStyle(wb);
+			row = sheet.createRow(ExcelUtil.getLocalRow());
+			for(int i=0; i<7; i++){
+				cell = row.createCell(i);
+				cell.setCellValue(head0[i]);
+				cell.setCellStyle(titleStyle);
+			}
+			
+			
+			ExcelUtil.increaseRow();// 换行//每个填充之间加入一个空行
+			row = sheet.createRow(ExcelUtil.getLocalRow());
+			Integer[] merges = new Integer[]{0,1,6};
+			List<Integer> mergeList = Arrays.asList(merges);
+			for(int i=0; i<7; i++){
+				if(mergeList.contains(i)){
+					
+				}else{
+					cell = row.createCell(i);
+					cell.setCellValue(head1[i]);
+				}
+				cell.setCellStyle(titleStyle);
+			}
+			
 			ExcelUtil.increaseRow();// 换行//每个填充之间加入一个空行
 
 			ExcelUtil.drawExcel(users, wb, sheet, User.class);
@@ -59,6 +102,7 @@ public class ExportExcelController {
 			wb.write(outputStream);
 		} catch (Exception e) {
 			logger.error(e.getMessage());
+			e.printStackTrace();
 		}
 
 	}
